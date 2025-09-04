2f14226d3a290e6ae1ead4b8a210fab65c43d557
The Skip WTF <31883729+sechi2000@users.noreply.github.com>
2025-09-04 10:43:49 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
-import Gate from './Gate'
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Gate />
-  </React.StrictMode>
-)
+import App from './App'
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>)
