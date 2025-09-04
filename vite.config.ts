17251b243b51c1259d29d14d625250ca7ac630ad
The Skip WTF <31883729+sechi2000@users.noreply.github.com>
2025-09-04 15:01:26 +0200

    Actualizar vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite'
-export default defineConfig({ base: '/js_torneos/'  , build: { outDir: 'docs' } })
+export default defineConfig({ base: 'js_torneos/'  , build: { outDir: 'docs' } })
 export const build = {};
